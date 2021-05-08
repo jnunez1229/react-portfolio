7d9f2239030f7878bc2d6a4f67492c9455075aa6
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-05-08 11:13:06 -0500

    update Contact with hooks

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,45 +1,86 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {validateEmail} from '../../utils/helpers'
 
 function Contact(){
+
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message} = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleChange(e){
+
+        if(e.target.name === 'email'){
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                if (!e.target.value.length) {
+                  setErrorMessage(`${e.target.name} is required.`);
+                } else {
+                  setErrorMessage('');
+                }
+            }
+        }
+
+        if(!errorMessage){
+            setFormState({...formState, [e.target.name]: e.target.value})
+        }      
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+
     return (
-        <div className="container mt-3">
-          <h2 class="text-center">Contact</h2>
+      <section>
+        <div  className="container mt-3">
+          <h2 data-testid='h2contact' className="text-center">Contact</h2>
+          <form id='contact-form' onSubmit={handleSubmit}>
           <div>
-            <div class="mb-3">
-              <label for="name" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label" >
                 Name
               </label>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="name"
                 placeholder="name"
+                defaultValue={name} onBlur={handleChange}
               />
             </div>
-            <div class="mb-3">
-              <label for="email" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="email"
                 placeholder="name@example.com"
+                defaultValue={email} onBlur={handleChange}
               />
             </div>
-            <div class="mb-3">
-              <label for="bio" class="form-label">
+            <div className="mb-3">
+              <label htmlFor="bio" className="form-label">
                 Message
               </label>
-              <textarea class="form-control" id="bio" rows="3"></textarea>
+              <textarea className="form-control" id="bio" rows="3" defaultValue={message} onBlur={handleChange}></textarea>
             </div>
-            <div class="col-12">
-              <button class="btn btn-primary" type="submit">
+            <div className="col-12">
+              <button className="btn btn-primary" type="submit">
                 Submit
               </button>
             </div>
           </div>
+          </form>
         </div>
+      </section>
+        
     );
 }
 
